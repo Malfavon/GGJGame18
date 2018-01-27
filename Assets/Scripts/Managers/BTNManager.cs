72b5d28bbff98bd7cec14c0c7ee38311b4c29304
abrahamones <31054212+abrahamones@users.noreply.github.com>
2018-01-27 08:09:47 -0600

    Movimiento de personaje listo
    Ya está la cámara y el movimiento de personajes

---
+++ Assets/Scripts/Managers/BTNManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BTNManager : MonoBehaviour {
+
+
+	public void NewGameBtn (string newGameLevel) 
+	{
+		SceneManager.LoadScene (newGameLevel);	
+	}
+
+	public void ExitGameBtn () 
+	{
+		Application.Quit ();
+	}
+
+
+
+}
